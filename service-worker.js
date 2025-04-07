0873c967dfa4ae0aa43a26efc9bd9cb16ef3c2ef
jasonyyy03 <101031915+jasonyyy03@users.noreply.github.com>
2025-04-07 23:13:04 +0800

    add

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+// service-worker.js
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('v1').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/static/css/bootstrap.min.css',
+          '/static/js/bootstrap.bundle.min.js'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
